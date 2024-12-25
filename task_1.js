2dd3a9fff0f0e206240e87c13095c26ff40c2bea
gigilya <gigilya1133@gmail.com>
2024-12-25 12:09:23 +0300

    add task 1,2,4,5,7,9

--- task_1.js
+++ task_1.js
@@ -1,1 +1,13 @@
 // Написать функцию, которая на вход принимает целое число и возвращает факториал этого числа
+
+function factorial(integer) {
+  let fact = 1;
+  let i = 1;
+
+  for (; integer + 1 > i; i++) {
+    fact *= i;    
+  }
+  return fact;
+}
+
+factorial(4);
--- task_7.js
+++ task_7.js
@@ -1,2 +1,7 @@
 // Напишите функцию, которая принимает массив в качестве входных данных и удаляет из него все "ложные значения"
 // ( https://developer.mozilla.org/ru/docs/%D0%A1%D0%BB%D0%BE%D0%B2%D0%B0%D1%80%D1%8C/Falsy )
+
+let falseValue = (arr) => arr.filter(Boolean);
+
+const testArray1 = [1, false, 2, "", 3, null, 0, undefined, NaN, 4];
+falseValue(testArray1);
